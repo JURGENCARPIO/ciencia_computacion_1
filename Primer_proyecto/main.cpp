856c808119619367d7ccb30762c221b324e45138
JURGENCARPIO <jurgen.carpio@ucsp.edu.pe>
2017-03-21 10:38:48 -0500

    Add files via upload

--- Primer_proyecto/main.cpp
+++ Primer_proyecto/main.cpp
@@ -4,6 +4,45 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x;
+    float y;
+
+    cout<<"ingrese el valor de x:";
+    cin >> x;
+    cout<<"ingrese el valor de y:";
+    cin >> y;
+
+    float j=x + y;
+
+    cout << "la suma de " <<x<<"+"<<y<<" es :"<<j<< endl;
+
+    cout<<"ingrese el valor de x:";
+    cin >> x;
+    cout<<"ingrese el valor de y:";
+    cin >> y;
+
+    float u=x - y;
+
+    cout << "la resta de "<<x<<"-"<<y<<" es: "<<u<< endl;
+
+    cout<<"ingrese el valor de x:";
+    cin >> x;
+    cout<<"ingrese el valor de y:";
+    cin >> y;
+
+    float r=x * y;
+
+    cout << "la multiplicacion de "<<x<<"*"<<y<<" es: "<<r<< endl;
+
+    cout<<"ingrese el valor de x:";
+    cin >> x;
+    cout<<"ingrese el valor de y:";
+    cin >> y;
+
+    float g=x / y;
+
+    cout << "la division de "<<x<<"/"<<y<<" es: "<<g<< endl;
     return 0;
 }
+
+
